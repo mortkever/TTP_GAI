1dbf7cb4e33a782d91987a4ab2bf716e3c73bb74
CianOlieslagers <cianolieslagers.sji@hotmail.be>
2025-02-17 12:17:50 +0100

    Basis Datastructuur RoundRobinStructuur

---
+++ Match.java
@@ -0,0 +1,25 @@
+public class Match {
+    private Team team1;
+    private Team team2;
+
+    public Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+}
---
+++ Team.java
@@ -0,0 +1,16 @@
+public class Team {
+    private String name;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
