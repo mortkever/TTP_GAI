2c2109807122d9946460ed1447aa3791826642d6
Victor <57635691+mortkever@users.noreply.github.com>
2025-03-10 12:00:06 +0100

    cleanup

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) throws GRBException {
 
         String fileName = "Data/NL4.xml";
+        int upperbound = 3;
 
         // Put the table in a 2D array
         InputHandler inputHandler = new InputHandler(fileName);
@@ -23,6 +24,7 @@ public class Main {
         int timeSlots = 2 * nTeams;
 
         // Print the 2D array
+        PrintHandler printHandler = new PrintHandler();
         printHandler.printDistanceMatrixContents(distanceMatrix);
 
         // ---------------------- Voorbeeld code --------------------------
@@ -41,17 +43,6 @@ public class Main {
         // Stap 6: Validate solution
         ScheduleValidator scheduleValidator = new ScheduleValidator(schedule);
         scheduleValidator.validate();
-
-        GRBModel model = new GRBModel(new GRBEnv());
-        model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
-
-        String fileName = "Data/NL4.xml";
-
-        // Put the table in a 2D array
-        InputHandler inputHandler = new InputHandler(fileName);
-        int[][] distanceMatrix = inputHandler.getDistanceMatrix();
-        int nTeams = distanceMatrix.length;
-        int timeSlots = 2 * nTeams;
 
         GRBModel model = new GRBModel(new GRBEnv());
         model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
@@ -75,21 +66,21 @@ public class Main {
         }
 
         // Contrait 1: FlowConversion
-        for (int t = 0; t < nTeams; t++){
-            for(int i =0; i< nTeams; nTeams++){
-                for(int s =0; s<timeSlots;timeSlots++){
+        for (int t = 0; t < nTeams; t++) {
+            for (int i = 0; i < nTeams; nTeams++) {
+                for (int s = 1; s < timeSlots; timeSlots++) {
 
                     GRBLinExpr flow = new GRBLinExpr();
 
-                    for(int j = 0; j <nTeams; j++){
-                        if(i != j){
-                            flow.addTerm(1, x[t][s - 1][j][i]);  // Kwam van team j naar i op s-1
+                    for (int j = 0; j < nTeams; j++) {
+                        if (i != j) {
+                            flow.addTerm(1, x[t][s - 1][j][i]); // Kwam van team j naar i op s-1
                         }
                     }
 
                     for (int j = 0; j < nTeams; j++) {
                         if (i != j) {
-                            flow.addTerm(-1, x[t][s][i][j]);  // Vertrekt van i naar team j op s
+                            flow.addTerm(-1, x[t][s][i][j]); // Vertrekt van i naar team j op s
                         }
                     }
                     model.addConstr(flow, GRB.EQUAL, 0, "flow_conservation(t=" + t + ",i=" + i + ",s=" + s + ")");
@@ -100,8 +91,8 @@ public class Main {
 
         // Constraint 5 :
 
-        for (int s = 0; s < nTeams; s++) {  // Voor elk team
-            for (int t = 0; t < timeSlots; t++) {  // Voor elk tijdslot
+        for (int s = 0; s < nTeams; s++) { // Voor elk team
+            for (int t = 0; t < timeSlots; t++) { // Voor elk tijdslot
                 GRBLinExpr constraint = new GRBLinExpr();
 
                 // Eerste som: Flow die vertrekt van i naar j op tijdstip t
@@ -113,7 +104,7 @@ public class Main {
 
                 // Tweede som: Flow vanuit andere tijdstippen t' naar hetzelfde team s
                 for (int t2 = 0; t2 < timeSlots; t2++) {
-                    if (t2 != t) {  // Vermijd dubbele toewijzing
+                    if (t2 != t) { // Vermijd dubbele toewijzing
                         for (int j = 0; j < nTeams; j++) {
                             constraint.addTerm(1, x[t2][s][j][s]);
                         }
@@ -124,12 +115,6 @@ public class Main {
                 model.addConstr(constraint, GRB.EQUAL, 1, "flow_constraint_s" + s + "_t" + t);
             }
         }
-
-        // Constraint 6
-
-
-
-
 
         // constraint 3
         for (int t = 0; t < nTeams; t++) {
@@ -158,5 +143,7 @@ public class Main {
         }
 
         model.optimize();
+
+        
     }
 }
